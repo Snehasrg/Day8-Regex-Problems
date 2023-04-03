bd6c50379c7104bb494b3477d5616ecddc29c02c
Snehasrg <sghongade642@gmail.com>
2023-04-03 15:44:17 +0530

    [sneha]Added program for valid website address

--- ValidateRegex.cs
+++ ValidateRegex.cs
@@ -72,5 +72,21 @@ namespace RegexPattern
                 Console.WriteLine("Invalid file name");
             }
         }
+
+        public static void WebsiteAddress()
+        {
+            Regex regex = new Regex(@"^(http|http(s)?://)?([\w-]+\.)+[\w-]+[.com|.in|.org]+(\[\?%&=]*)?");
+            Console.WriteLine("Enter a url ");
+            string Website = Console.ReadLine();
+            if (regex.IsMatch(Website))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Not valid");
+            }
+
+        }
     }
 }
