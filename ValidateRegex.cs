ddf40e542c1d0ac5e3767fc3107b4b3f95847227
Snehasrg <sghongade642@gmail.com>
2023-04-03 15:39:07 +0530

    [sneha]Added program for valid file name

--- ValidateRegex.cs
+++ ValidateRegex.cs
@@ -56,5 +56,21 @@ namespace RegexPattern
             Console.WriteLine("The Count of given Pattern in given String " + matches.Count);
 
         }
+
+        public static void FileExtension()
+        {
+            Regex regex = new Regex(@"([^\s]+(\.(?i)(jpe?g|png|gif|bmp))$)");
+
+            Console.WriteLine("Enter filename with image file extension : ");
+            string filename = Console.ReadLine();
+            if (regex.IsMatch(filename))
+            {
+                Console.WriteLine("Valid file name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid file name");
+            }
+        }
     }
 }
